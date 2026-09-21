4440e9c0ccdf0144a54d46bd2b95fd66f7539a29
ConsciousBone <68429595+ConsciousBone@users.noreply.github.com>
2026-09-21 02:14:50 +0100

    finish level2 design, add numkeys jump to levels

--- scripts/world.gd
+++ scripts/world.gd
@@ -7,3 +7,12 @@ func _unhandled_input(event: InputEvent) -> void:
 	if Input.is_action_just_pressed("esc"):
 		print("esc pressed, going to title")
 		get_tree().change_scene_to_file("res://scenes/title.tscn")
+	if Input.is_action_just_pressed("1"):
+		print("going to level 0")
+		get_tree().change_scene_to_file("res://scenes/level0.tscn")
+	if Input.is_action_just_pressed("2"):
+		print("going to level 1")
+		get_tree().change_scene_to_file("res://scenes/level1.tscn")
+	if Input.is_action_just_pressed("3"):
+		print("going to level 2")
+		get_tree().change_scene_to_file("res://scenes/level2.tscn")
