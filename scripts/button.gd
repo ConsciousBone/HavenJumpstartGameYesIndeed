b628396b881d48f959665b78f605b80046a68ff7
ConsciousBone <68429595+ConsciousBone@users.noreply.github.com>
2026-09-21 02:35:34 +0100

    add end screen

--- scripts/button.gd
+++ scripts/button.gd
@@ -1,5 +1,9 @@
 extends Control
 
 func _on_start_button_pressed() -> void:
-	print("start button pressed, moving to world0")
+	print("start button pressed, moving to level0")
 	get_tree().change_scene_to_file("res://scenes/level0.tscn")
+	
+func _on_title_button_pressed() -> void:
+	print("title button pressed, moving to title")
+	get_tree().change_scene_to_file("res://scenes/title.tscn")
