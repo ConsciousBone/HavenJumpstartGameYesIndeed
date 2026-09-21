a43d5057143d0139b9f384eb400cb124485c2156
ConsciousBone <68429595+ConsciousBone@users.noreply.github.com>
2026-09-21 02:20:17 +0100

    add level text to levels

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 
 const SPEED = 200.0
-const JUMP_VELOCITY = -500.0
+const JUMP_VELOCITY = -450.0
 		
 func _ready() -> void:
 	print("player ready, adding to player group")
