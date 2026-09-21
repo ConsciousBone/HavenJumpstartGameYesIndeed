febbf7ffd49f9c20ac3064fdaca4ca64d3bd9c40
ConsciousBone <68429595+ConsciousBone@users.noreply.github.com>
2026-09-21 03:23:24 +0100

    add/fix coin logic

--- scenes/coin.gd
+++ scenes/coin.gd
@@ -2,5 +2,6 @@ extends Area2D
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
+		print("collecting coin")
 		GameManager.collect_coin()
 		queue_free()
--- scripts/world.gd
+++ scripts/world.gd
@@ -1,9 +1,16 @@
 extends Node2D
 
+@onready var player = $Player
+var player_start_position: Vector2
+
+func _ready() -> void:
+	player_start_position = player.global_position
+
 func _unhandled_input(event: InputEvent) -> void:
 	if Input.is_action_just_pressed("backspace"):
 		print("backspace pressed, resetting")
-		get_tree().reload_current_scene()
+		player.global_position = player_start_position
+		player.velocity = Vector2.ZERO
 	if Input.is_action_just_pressed("esc"):
 		print("esc pressed, going to title")
 		get_tree().change_scene_to_file("res://scenes/title.tscn")
